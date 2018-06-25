8059e7c291542de7614140ad54ec222fe62ba897
Julian Notebook <jgi@informatik.uni-kiel.de>
2018-06-25 13:08:47 +0200

    Text Stylings & Home

--- app/controllers/ApplicationController.java
+++ app/controllers/ApplicationController.java
@@ -12,4 +12,31 @@ public class ApplicationController extends Controller {
     public Result home() {
         return ok(home.render());
     }
+
+    public Result platforms() {
+        return ok(platforms.render());
+    }
+
+    public Result aggregators() {
+        return ok(aggregators.render());
+    }
+
+
+    public Result howTo() {
+        return ok(howto.render());
+    }
+
+    public Result about() {
+        return ok(about.render());
+    }
+
+    public Result management() {
+        return ok(management.render());
+    }
+
+    public Result search() {
+        return ok(search.render());
+    }
+
+
 }
