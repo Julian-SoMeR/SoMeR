253577ad7fb52c7b2c2d74509db9c30886636798
Julian <38381587+Julian-SoMeR@users.noreply.github.com>
2018-08-03 22:45:55 +0200

    Added search highlighting. Now the database needs a rework...

---
+++ public/javascripts/search.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+let searchContainer = document.getElementById("search-container");
+searchContainer.style.backgroundColor = "#F5F5F5";
+searchContainer.style.border = "1px solid black";
+searchContainer.style.borderBottom = "1.25px solid #F5F5F5";
+searchContainer.style.transitionDuration = "unset";
+});
